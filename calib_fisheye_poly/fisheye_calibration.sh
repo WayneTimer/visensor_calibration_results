3ccfce27e6f18a7098d651300af218ff4cb940b4
WayneTimer (LINYI@mav-workstation) <ylinax@connect.ust.hk>
2017-03-15 20:59:48 +0800

    Add fisheye_ploy_model calibration related.

---
+++ calib_fisheye_poly/fisheye_calibration.sh
@@ -0,0 +1,6 @@
+rosrun camera_model Calibration --camera-name <name> -i <image_path> -p left- -e png -w 9 -h 8 --size 70 --camera-model myfisheye -n 120
+
+
+is_fast: 0 -> 1
+last two: 4000
+last one: 110
